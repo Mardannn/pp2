5a07d26917a96bb3d9033e4e8ac259934d0f8939
Mardannn <m.bolatbekk.20@mail.ru>
2025-01-22 16:27:44 +0500

    Add files via upload

---
+++ mz.py
@@ -0,0 +1,31 @@
+print("hello World")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# Data types
+x = 3
+print(type(x))
+
+
+x = 1
+y = 1.2
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = int(1)   # x will be 1
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
